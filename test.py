6ec19580efae666697fa03eeefe738326480e5e0
Takkun <takkun.053@gmail.com>
2021-03-15 20:31:42 +0900

    忘れてた()

--- test.py
+++
@@ -1,6 +0,0 @@
-import sys,requests,configparser,json,os,zipfile
-
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)),"config.ini"),encoding="utf-8")
-
-print(type(config["install"]["delete_zip"]))
